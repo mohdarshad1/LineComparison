bad1c8b8abf61351b343d310a1f0136375ca9bb0
mohdarshad1 <mdarshad181@gmail.com>
2021-04-18 14:41:40 +0530

    [Mohd Arshad] : Added UC2

--- LineComparison.java
+++ LineComparison.java
@@ -1,26 +1,41 @@
 import java.util.*;
 public class LineComparison {
-
-	public static void main(String[] args) 
+	double LineLength()
 	{
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Welcome To Line Comparison Computation Program");
-		System.out.println("Enter the x-coordinate of 1st point");
 		int x1=sc.nextInt(); 
-		
-		System.out.println("Enter the x-coordinate of 2nd point");
 		int x2=sc.nextInt();
-		
-		System.out.println("Enter the y-coordinate of 1st point");
 		int y1=sc.nextInt();
-		
-		System.out.println("Enter the y-coordinate of 2nd point");
 		int y2=sc.nextInt();
 	    int x=(x2-x1)*(x2-x1);
 	    int y=(y2-y1)*(y2-y1);
 		double length= Math.sqrt(x+y);
+		return length;
+	}
+	
+	void equal()
+	{
+		System.out.println("Enter Coordinates of the First Line");
+		String p=String.valueOf(LineLength());
+		System.out.println("Enter Coordinates of the second Line");
+		String q=String.valueOf(LineLength());
+		if(p.equals(q))
+		{
+			System.out.println("Lines are equal");
 		
-		System.out.println("Length of the line is:"+length);
+		}
+		else
+		{
+		System.out.println("Line are not equal");
+		}
+	}
+	
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome to Line Comparison Computation Program ");
+		LineComparison lc=new LineComparison();
+		Scanner sc=new Scanner(System.in);
+		lc.equal();
 	}
 	
 
